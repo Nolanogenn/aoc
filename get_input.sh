91837388dfe5760f76a011d3a42efc0c4c4c7952
Gennaro Nolano <nolanogenn@gmail.com>
2024-10-25 10:29:26 +0200

    fix template

--- get_input.sh
+++ get_input.sh
@@ -2,10 +2,13 @@ SESSION=$(cat ~/vboxshare/aoc_session.txt)
 YEAR=$1
 DAY=$2
 
-if [ ! -d "$YEAR/$DAY" ];
+if [ ! -d "$YEAR/day$DAY" ];
 then
 	mkdir $YEAR/day$DAY
 fi
 curl https://adventofcode.com/$YEAR/day/$DAY/input --cookie "session=$SESSION" > "$YEAR/day$DAY/in"
-cp template.py $YEAR/day$DAY/solve.py
+if [ ! -f "$YEAR/day$DAY/solve.py"];
+then
+	cp template.py $YEAR/day$DAY/solve.py
+fi
 
