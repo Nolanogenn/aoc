528091462a2ef19d0554752a28ed4ea9b7b86a96
Gennaro Nolano <nolanogenn@gmail.com>
2025-11-27 12:00:50 +0100

    day4 2015 with elixir

--- 2015/day4/solve.ex
+++ 2015/day4/solve.ex
@@ -2,26 +2,33 @@ defmodule Solve do
   def solve(filename) do
     {:ok, contents} = File.read(filename)
     sol1 = contents
-        |> String.split("\n", trim: true)
-        |> Enum.map(&String.to_integer/1)
-        |> Enum.map(&part1/1)
-        |> Enum.sum()
+        |> String.replace("\n", "")
+        |> part1(0)
     IO.puts "Part 1 solution: "
     IO.inspect sol1
     sol2 = contents
-        |> String.split("\n", trim: true)
-        |> Enum.map(&String.to_integer/1)
-        |> Enum.map(&part2/1)
-        |> Enum.sum()
+        |> String.replace("\n", "")
+        |> part2(0)
     IO.puts "Part 2 solution: "
     IO.inspect sol2
   end
-  def part1(num) do
-    num
+  def check(hash, startingWith) do
+    String.starts_with?(hash, startingWith)
   end
-  def part2(num) do
-    num
+  def part1(password, num) do
+    hashed = :crypto.hash(:md5, password <> Integer.to_string(num)) |> Base.encode16()
+    case check(hashed, "00000") do
+      true -> num
+      _ -> part1(password, num+1)
+    end
+  end
+  def part2(password, num) do
+    hashed = :crypto.hash(:md5, password <> Integer.to_string(num)) |> Base.encode16()
+    case check(hashed, "000000") do
+      true -> num
+      _ -> part2(password, num+1)
+    end
   end
 end
 
-Solve.solve("test")
+Solve.solve("in")
