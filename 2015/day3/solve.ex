72d9c1b97337154302a9a8926ff5295356babf6d
Gennaro Nolano <nolanogenn@gmail.com>
2025-11-27 11:24:24 +0100

    day 3 with elixir 2015

--- 2015/day3/solve.ex
+++ 2015/day3/solve.ex
@@ -2,26 +2,56 @@ defmodule Solve do
   def solve(filename) do
     {:ok, contents} = File.read(filename)
     sol1 = contents
-        |> String.split("\n", trim: true)
-        |> Enum.map(&String.to_integer/1)
-        |> Enum.map(&part1/1)
-        |> Enum.sum()
+        |> String.split("", trim: true)
+        |> part1({0,0}, [{0,0}])
     IO.puts "Part 1 solution: "
-    IO.puts sol1
+    IO.inspect sol1
     sol2 = contents
-        |> String.split("\n", trim: true)
-        |> Enum.map(&String.to_integer/1)
-        |> Enum.map(&part2/1)
-        |> Enum.sum()
+        |> String.split("", trim: true)
+        |> Enum.with_index()
+        |> part2({0,0},{0,0},[{0,0}])
     IO.puts "Part 2 solution: "
     IO.puts sol2
   end
-  def part1(num) do
-    num
+  def part2([{nextMove, idx}| movs], posSanta, posBot, visited) do
+    {newPosSanta, newPosBot, newVisited} = 
+    if rem(idx,2) == 0 do
+      newPosBot = move(nextMove, posBot)
+      newVisited = addIfNotPresent(visited, newPosBot)
+      {posSanta, newPosBot, newVisited}
+    else
+      newPosSanta = move(nextMove, posSanta)
+      newVisited = addIfNotPresent(visited, newPosSanta)
+      {newPosSanta, posBot, newVisited}
+    end
+    part2(movs, newPosSanta, newPosBot, newVisited)
   end
-  def part2(num) do
-    num
+  def part2([], _, _, visited) do
+    length(visited)  
+  end
+  def addIfNotPresent(t, newValue) do
+    case Enum.member?(t, newValue) do
+      true -> t
+      _ -> [newValue|t]
+    end
+  end
+  def move(dir, currPos) do
+    case dir do 
+      ">" -> {elem(currPos,0)+1, elem(currPos,1)}
+      "<" -> {elem(currPos,0)-1, elem(currPos,1)}
+      "v" -> {elem(currPos,0), elem(currPos,1)-1}
+      "^" -> {elem(currPos,0), elem(currPos,1)+1}
+      _ -> currPos
+    end
+  end
+  def part1([nextMov|movs], currPos, visited) do
+    nextPos = move(nextMov, currPos)
+    newVisited = addIfNotPresent(visited, nextPos)
+    part1(movs, nextPos, newVisited)
+  end
+  def part1([], _, visited) do
+    length(visited)
   end
 end
 
-Solve.solve('test')
+Solve.solve("in")
