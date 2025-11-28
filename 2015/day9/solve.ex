750e8ecf683225830f222d9c44bda05f1a79cf3b
Gennaro Nolano <nolanogenn@gmail.com>
2025-11-28 17:20:29 +0100

    2015 day9 w/ elixir

--- 2015/day9/solve.ex
+++ 2015/day9/solve.ex
@@ -6,13 +6,11 @@ defmodule Solve do
         |> part1()
     IO.puts "Part 1 solution: "
     IO.inspect sol1
-#    sol2 = contents
-#        |> String.split("\n", trim: true)
-#        |> Enum.map(&String.to_integer/1)
-#        |> Enum.map(&part2/1)
-#        |> Enum.sum()
-#    IO.puts "Part 2 solution: "
-#    IO.inspect sol2
+    sol2 = contents
+        |> String.split("\n", trim: true)
+        |> part2()
+    IO.puts "Part 2 solution: "
+    IO.inspect sol2
   end
   def handleDistance([], locations, pairs) do
     {locations |> Enum.uniq(), pairs}
@@ -30,67 +28,37 @@ defmodule Solve do
   def handleDistances(distances) do
     handleDistance(distances, [],[])
   end
-  def findAllRoutes([], _, _, cost) do
-    cost
+  def getPaths([]) do
+    [[]]
   end
-  def findAllRoutes([current|ls], locations, pairs, cost) do
-    IO.puts("FIND ALL ROUTES")
-    IO.inspect({current, cost})
-    minimalCost = findMinimalCost(current, pairs, cost)
-    case minimalCost < cost do
-      true -> findAllRoutes(ls, locations, pairs, minimalCost)
-      _ -> findAllRoutes(ls, locations, pairs, cost)
-    end
-  end
-  def findAllRoutes(locations, pairs) do
-    findAllRoutes(locations, locations, pairs, 999999)
-  end
-  def findMinimalCost(starting, pairs, minimal) do
-    visitable = getRoutesStartingFrom(starting,pairs,[])
-    costs = findAllRoutesFrom(pairs, [{[starting],visitable,0}],[], minimal)
-    Enum.min(costs)
-  end
-  def findAllRoutesFrom(pairs, [{visited,tovisit,currCost}|ps], costs, minimal) do
-    next = nextStep(pairs, visited, tovisit, currCost, minimal)
-    case next do
-      [] -> findAllRoutesFrom(pairs, ps, [currCost+findCost(Enum.at(visited,0),Enum.at(tovisit,0),pairs)|costs], minimal)
-      _ -> findAllRoutesFrom(pairs, next++ps, costs, minimal)
-    end
-  end
-  def findAllRoutesFrom(_, [], costs, _) do
-    costs
-  end
-  def nextStep(pairs, visited, tovisit, currCost, minimal) do
-    nextStep(pairs, visited, tovisit, currCost, [], minimal)
-  end
-  def nextStep(_, _, [], _, toret, _) do
-    toret
-  end
-  def nextStep(pairs, visited, [n|ns], currCost, toret, minimal) do
-    routes = getRoutesStartingFrom(n, pairs, visited)
-    toret = addRoutes(Enum.at(visited,0), routes, toret, pairs, currCost, visited, n, minimal)
-    nextStep(pairs, visited, ns, currCost, toret, minimal)
-  end
-  def addRoutes(starting, [r|rs], toret, pairs, currCost, visited, to, minimal) do
-    newVisited = [r|visited]
-    newCost = currCost + findCost(starting, to,pairs)
-    case newCost >= minimal do
-      true -> addRoutes(starting, rs, toret, pairs, currCost, visited, to, minimal)
-      _ ->addRoutes(starting, rs,[{newVisited, getRoutesStartingFrom(r, pairs, visited), currCost+findCost(starting,to,pairs)}|toret], pairs, currCost, visited, to, minimal)   end
-  end
-  def addRoutes(_, [], toret, _, _, _,_, _) do
-    toret
-  end
-  def getRoutesStartingFrom(starting,pairs,visited) do
-    Enum.filter(pairs, fn {{x,y}, _} -> x == starting and !Enum.member?(visited,y) end) |> Enum.map(&elem(&1,0)) |> Enum.map(&elem(&1,1)) 
+  def getPaths(x) do
+    for elem <- x,
+      rest = x -- [elem],
+      p <- getPaths(rest),
+      do: [elem | p]
   end
   def findCost(from, to, list) do
     Enum.filter(list, fn {{x,y}, _} -> x == from and y == to end)
     |> Enum.map(&elem(&1,1)) |> Enum.at(0)
   end
+  def computeCost(path, pairs) do
+    computeCost(path, pairs,0) 
+  end
+  def computeCost([_], _, cost) do
+    cost
+  end
+  def computeCost([c,n|cs], pairs, cost) do
+    computeCost([n|cs], pairs, cost+findCost(c,n,pairs))
+  end
   def part1(distances) do
     {locations, pairs} = handleDistances(distances)
-    findAllRoutes(locations, pairs)
+    paths = getPaths(locations)
+    Enum.map(paths, fn x -> computeCost(x, pairs) end) |> Enum.min()
+  end
+  def part2(distances) do
+    {locations, pairs} = handleDistances(distances)
+    paths = getPaths(locations)
+    Enum.map(paths, fn x -> computeCost(x, pairs) end) |> Enum.max()
   end
 end
 
