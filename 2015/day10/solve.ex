88ce6480e651009063420893fe5800ca59e6e97e
Gennaro Nolano <nolanogenn@gmail.com>
2025-12-01 10:55:42 +0100

    2015 day10 w/ elixir

--- 2015/day10/solve.ex
+++ 2015/day10/solve.ex
@@ -1,37 +1,59 @@
 defmodule Solve do
   def solve(filename) do
     {:ok, contents} = File.read(filename)
-    sol1 = contents
-        |> String.replace("\n", "")
-        |> part1(3)
+    parsed = contents |> String.replace("\n", "")
+    sol1 = part1(parsed,40,%{})
     IO.puts "Part 1 solution: "
     IO.inspect sol1
-#    sol2 = contents
-#        |> String.split("\n", trim: true)
-#        |> Enum.map(&String.to_integer/1)
-#        |> Enum.map(&part2/1)
-#        |> Enum.sum()
-#    IO.puts "Part 2 solution: "
-#    IO.inspect sol2
+    sol2 = part1(parsed,50,%{})
+    IO.puts "Part 2 solution: "
+    IO.inspect sol2
   end
-  def mapSequence([c|cs], out, lastSeen, n) do
-    case c == lastSeen do
-      true -> mapSequence(cs, out, lastSeen, n+1)
-      _ -> 
+  def groupSequence([c|cs]) do
+    groupSequence(cs, c, c, [])
+  end
+  def groupSequence([], _, currSeq, toret) do
+    [currSeq | toret]
+  end
+  def groupSequence([c|cs], currC, currSeq, toret) do 
+    case c == currC do
+      true -> groupSequence(cs, currC, currSeq <> c, toret)
+      _ -> groupSequence(cs, c, c, [currSeq|toret])
     end
   end
-  def mapsequence([], out, _) do
-    out
+  def joinTuple(t) do
+    Integer.to_string(elem(t,1)) <> elem(t,0)
   end
-  def part1(sequence,n) do
-    case n>0 do
-    true -> 
-      mapSequence(String.split(sequence, "", trim: true), "", "", 0)
-    _ ->
-      IO.inspect(sequence)
-      String.length(sequence)
-      end
+  def computeSeq(s) do
+    s
+    |> String.split("", trim: true)
+    |> Enum.frequencies()
+    |> Enum.to_list()
+    |> Enum.at(0)
+    |> joinTuple()
+  end
+  def genSeq([],s,m) do
+    {Enum.join(s),m}
+  end
+  def genSeq([s|ss],seqs, m) do
+    case Map.has_key?(m,s) do
+      true -> genSeq(ss, [Map.get(m,s) | seqs], m)
+      _ -> mappedSeq = computeSeq(s)
+          genSeq(ss, [mappedSeq | seqs], Map.update(m, s, mappedSeq, fn _ -> mappedSeq end))
     end
+  end
+  def genSeq(sequences, m) do
+    genSeq(sequences, [], m) 
+  end
+  def part1(sequence,n,m) do
+    case n > 0 do
+      true -> {newSeq, newM} = String.split(sequence, "", trim: true)
+              |> groupSequence()
+              |> genSeq(m)
+        part1(newSeq,n-1,newM)
+      _ -> String.length(sequence)
+    end
+  end
 end
 
-Solve.solve("test")
+Solve.solve("in")
