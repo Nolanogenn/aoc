c1b3c049ec3a4fdb913612753cd61e8da22eae17
Gennaro Nolano <nolanogenn@gmail.com>
2025-12-10 00:09:44 +0100

    sad day

--- 2025/day9/solve.py
+++ 2025/day9/solve.py
@@ -1,54 +1,41 @@
+import numpy as np
+import shapely
 import itertools
 
 def getArea(x,y):
-    h = abs(x[0] - y[0])+1
-    w = abs(x[1] - y[1])+1
+    h = abs(x[0]-y[0])+1
+    w = abs(x[1]-y[1])+1
     return h*w
 
-def crosses(pair, line):
-    wL = min((line[0][1], line[1][1]))
-    WL = max((line[0][1], line[1][1]))
-    hL = min((line[0][0], line[1][0]))
-    HL = max((line[0][0], line[1][0]))
-    h = min((pair[0][0], pair[1][0]))
-    H = max((pair[0][0], pair[1][0]))
-    w = min((pair[0][1], pair[1][1]))
-    W = max((pair[0][1], pair[1][1]))
-    if line[0][0] == line[1][0]:
-        if h < line[0][0] < H and (WL > w or wL < w):
-            return True
-        return False
-    if w < line[0][1] < W and (HL > h or wL < H):
-        return True
-    return False
+def gethw(x,y):
+    h = min((x[0], y[0]))
+    H = max((x[0], y[0]))
+    w = min((x[1], y[1]))
+    W = max((x[1], y[1]))
+    return h,H,w,W
 
-
-
-
-data = [x.strip().split(',') for x in open('test').readlines()]
+data = [x.strip().split(',') for x in open('in').readlines()]
 tiles = [(int(x[0]), int(x[1])) for x in data]
-tilesPairs = itertools.combinations(tiles,2)
+x = sorted([t[0] for t in tiles])
+y = sorted([t[1] for t in tiles])
 
-areas = [getArea(x[0], x[1]) for x in tilesPairs]
+tilesPairs = list(itertools.combinations(tiles,2))
+
+areas = [(getArea(x[0], x[1]),x) for x in tilesPairs]
 areas = sorted(areas)
-ans_1 = areas.pop()
+best = areas.pop()
+ans_1 = best[0]
+print(f"SOLUTION FOR PART1: {ans_1}")
 
 ans_2= 0
-tilesPairs = list(itertools.combinations(tiles,2))
-lines = [
-        x for x in tilesPairs
-        if (x[0][0] == x[1][0] or x[0][1] == x[1][1])
-        ]
-
-for pair in tilesPairs:
-    valid = True
-    for l in lines:
-        if crosses(pair, l):
-            print(pair,l)
-            valid = False
-    if valid:
-        print(pair, getArea(pair[0], pair[1]))
-
-
-print(f"SOLUTION FOR PART1: {ans_1}")
+polygon = shapely.Polygon(tiles)
+found = False
+while not found:
+    xmin,xmax,ymin,ymax = gethw(best[1][0], best[1][1])
+    geom = shapely.box(xmin,ymin,xmax,ymax)
+    if shapely.contains(polygon, geom):
+        found = True
+    else:
+        best = areas.pop()
+ans_2 = best[0]
 print(f"SOLUTION FOR PART2: {ans_2}")
